c3914d35b160ed36962cd451e38e791922c2aeb6
Patrik Svensson <patrik@patriksvensson.se>
2020-05-03 22:42:05 +0200

    Fix edit link

--- Program.cs
+++ Program.cs
@@ -16,13 +16,7 @@ namespace Statiqdev
                 .CreateWeb(args)
                 .AddSetting(Keys.Host, "duckhq.org")
                 .AddSetting(Keys.LinksUseHttps, true)
-                .AddSetting(
-                    Keys.DestinationPath,
-                    Config.FromDocument(doc => doc.Source.Parent.Segments.Last().SequenceEqual("posts".AsMemory())
-                        ? new NormalizedPath("blog")
-                            .Combine(doc.GetDateTime(WebKeys.Published).ToString("yyyy/MM/dd"))
-                            .Combine(doc.Destination.FileName.ChangeExtension(".html"))
-                        : doc.Destination.ChangeExtension(".html")))
+                .AddSetting("EditLink", Config.FromDocument((doc, ctx) => "https://github.com/duckhq/docs/edit/develop/input" + doc.Source.GetRelativeInputPath()))
                 .AddSetting("EditLink", Config.FromDocument((doc, ctx) => new NormalizedPath("https://github.com/duckhq/docs/edit/develop/input").Combine(doc.Source.GetRelativeInputPath())))
                 .AddSetting(SiteKeys.SchemaUri, "https://raw.githubusercontent.com/duckhq/duck/master/schemas/v0.14.json")
                 .AddShortcode("JsonSchema", typeof(SchemaShortCode))
