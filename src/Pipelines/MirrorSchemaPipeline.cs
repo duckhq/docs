cd0a9b6340c94390cf9e4a9d97239a9701780a91
Patrik Svensson <patrik@patriksvensson.se>
2020-05-05 12:03:52 +0200

    Mirror JSON schema
    Closes #12

---
+++ src/Pipelines/MirrorSchemaPipeline.cs
@@ -0,0 +1,30 @@
+﻿using Statiq.Common;
+using Statiq.Core;
+using Statiq.Web.GitHub;
+
+namespace Duck.Pipelines
+{
+    public class MirrorSchemaPipeline : Pipeline
+    {
+        public MirrorSchemaPipeline()
+        {
+            InputModules = new ModuleList
+            {
+                new ExecuteConfig(Config.FromContext(ctx => 
+                {
+                    return new ReadWeb(ctx.Settings.GetString(Constants.SchemaUri));
+                }))
+            };
+            
+            ProcessModules = new ModuleList
+            {
+                new SetDestination("schema.json")
+            };
+
+            OutputModules = new ModuleList
+            {
+                new WriteFiles()
+            };
+        }
+    }
+}
